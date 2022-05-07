0280bb7c8e4af6f5007197b6c485d371338f5149
Nataly-tk <natasha.totska@yahoo.com>
2022-05-07 16:47:45 +0300

    Updates

---
+++ precache-manifest.cbd2f65c0f3db71b1e6c348e0b423a6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dada5f4d7fbd7711b1b03d9f46a0ebc3",
+    "url": "/currency-convertor/index.html"
+  },
+  {
+    "revision": "8ed11bb1db3a38699a51",
+    "url": "/currency-convertor/static/css/main.7b6bdeb9.chunk.css"
+  },
+  {
+    "revision": "fceb777e064ecbd3ecb9",
+    "url": "/currency-convertor/static/js/2.636560f2.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/currency-convertor/static/js/2.636560f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed11bb1db3a38699a51",
+    "url": "/currency-convertor/static/js/main.059fd054.chunk.js"
+  },
+  {
+    "revision": "68bc9dc6e9529ad97297",
+    "url": "/currency-convertor/static/js/runtime-main.0f990bee.js"
+  }
+]);
